74e32891a3d4b7beb3bcb4a4c1d33d42b021a5b4
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2025-05-28 20:17:15 +0700

    new

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import SignIn from "./pages/AuthPages/SignIn";
+import SignUp from "./pages/AuthPages/SignUp";
+import NotFound from "./pages/OtherPage/NotFound";
+import UserProfiles from "./pages/Grammar/UserProfiles";
+import Videos from "./pages/UiElements/Videos";
+import Images from "./pages/UiElements/Images";
+import Alerts from "./pages/UiElements/Alerts";
+import Badges from "./pages/UiElements/Badges";
+import Avatars from "./pages/UiElements/Avatars";
+import Buttons from "./pages/UiElements/Buttons";
+import LineChart from "./pages/Charts/LineChart";
+import BarChart from "./pages/Charts/BarChart";
+import User from "./pages/User/User";
+import FormElements from "./pages/ArticleAndVideos/Article";
+import Member from "./pages/Member/Member";
+import AppLayout from "./layout/AppLayout";
+import { ScrollToTop } from "./components/common/ScrollToTop";
+import Home from "./pages/Dashboard/Home";
+import Vocabulary from "./pages/Vocabulary/Vocabulary";
+import Article from "./pages/ArticleAndVideos/Article";
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          {/* Dashboard Layout */}
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Home />} />
+
+            {/* Others Page */}
+            <Route path="/grammar" element={<UserProfiles />} />
+            <Route path="/vocabulary" element={<Vocabulary />} />
+            <Route path="/member" element={<Member />} />
+
+            {/* Forms */}
+            <Route path="/article" element={<Article />} />
+
+            {/* Tables */}
+            <Route path="/user" element={<User />} />
+
+            {/* Ui Elements */}
+            <Route path="/alerts" element={<Alerts />} />
+            <Route path="/avatars" element={<Avatars />} />
+            <Route path="/badge" element={<Badges />} />
+            <Route path="/buttons" element={<Buttons />} />
+            <Route path="/images" element={<Images />} />
+            <Route path="/videos" element={<Videos />} />
+
+            {/* Charts */}
+            <Route path="/line-chart" element={<LineChart />} />
+            <Route path="/bar-chart" element={<BarChart />} />
+          </Route>
+
+          {/* Auth Layout */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
---
+++ src/pages/Vocabulary/Vocabulary.tsx
@@ -0,0 +1,14 @@
+import { useState, useRef, useEffect } from "react";
+
+
+const Vocabulary: React.FC = () => {
+
+
+  return (
+    <div>
+      <p> Vocabulary</p>
+    </div>
+  );
+};
+
+export default Vocabulary;
