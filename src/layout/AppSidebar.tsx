2d3c78723ba42d42eb5f6d23026ca1eca32cf743
letrungviet2005 <vitprofb@gmail.com>
2025-06-21 23:07:33 +0700

    fix(ui): fix videos

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -85,7 +85,6 @@ const othersItems: NavItem[] = [
       { name: "Badge", path: "/badge", pro: false },
       { name: "Buttons", path: "/buttons", pro: false },
       { name: "Images", path: "/images", pro: false },
-      { name: "Videos", path: "/videos", pro: false },
     ],
   },
   {
--- src/pages/ArticleAndVideos/Video.tsx
+++ src/pages/ArticleAndVideos/Video.tsx
@@ -61,31 +61,32 @@ const Video: React.FC = () => {
         />
       </div>
 
-      {videos.map((video) => (
-        <div
-          key={video.id}
-          className="mb-8 border border-gray-300 rounded-lg shadow p-4"
-        >
-          <h2 className="text-xl font-semibold mb-2">{video.title}</h2>
-          <p className="text-sm text-gray-600 mb-1">{video.description}</p>
-          <p className="text-sm text-gray-500 mb-2">
-            ⏱️ Thời lượng: {video.duration}
-          </p>
-          <video
-            src={video.url}
-            controls
-            className="w-full max-h-[400px] mb-3 rounded"
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {videos.map((video) => (
+          <div
+            key={video.id}
+            className="border border-gray-300 rounded-lg shadow p-4 flex flex-col"
           >
-            <track src={video.subtitle} kind="subtitles" label="Phụ đề" />
-            Trình duyệt của bạn không hỗ trợ video.
-          </video>
-          <div className="text-xs text-gray-500">
-            <p>📂 Danh mục: {video.category.name}</p>
-            <p>🗓️ Ngày đăng: {new Date(video.createdAt).toLocaleString()}</p>
+            <h2 className="text-xl font-semibold mb-2">{video.title}</h2>
+            <p className="text-sm text-gray-600 mb-1">{video.description}</p>
+            <p className="text-sm text-gray-500 mb-2">
+              ⏱️ Thời lượng: {video.duration}
+            </p>
+            <video
+              src={video.url}
+              controls
+              className="w-full max-h-[400px] mb-3 rounded"
+            >
+              <track src={video.subtitle} kind="subtitles" label="Phụ đề" />
+              Trình duyệt của bạn không hỗ trợ video.
+            </video>
+            <div className="text-xs text-gray-500">
+              <p>📂 Danh mục: {video.category.name}</p>
+              <p>🗓️ Ngày đăng: {new Date(video.createdAt).toLocaleString()}</p>
+            </div>
           </div>
-        </div>
-      ))}
-
+        ))}
+      </div>
       {/* Phân trang */}
       <div className="flex justify-center items-center gap-4 mt-6">
         <button
